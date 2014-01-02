854203d7dc9a7b317ba46ed9a982d0a7db54c480
arisin <monokesi4120@gmail.com>
2014-01-02 19:09:19 +0900

    support time++ alias for Darwin

--- .zshrc.d/alias.datetime.zsh
+++ .zshrc.d/alias.datetime.zsh
@@ -2,5 +2,9 @@ alias date.minutes="date --iso-8601=minutes"
 alias date.date="date --iso-8601=date"
 
 alias ntpdate-hwclock="sudo ntpdate 210.173.160.27 && sudo hwclock --systohc"
+if [ `uname -s` = Darwin ]; then
+  alias time++="/usr/bin/time -lp"
+else
+  alias time++="/usr/bin/time --verbose"
+fi
 
-alias time++="/usr/bin/time --verbose"
