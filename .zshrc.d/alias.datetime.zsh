cd5d79a9bf9c7e7cd0937087a2cd394bf12a9e00
arisin <monokesi4120@gmail.com>
2014-01-02 19:27:32 +0900

    support date alias for Darwin

--- .zshrc.d/alias.datetime.zsh
+++ .zshrc.d/alias.datetime.zsh
@@ -1,5 +1,12 @@
-alias date.minutes="date --iso-8601=minutes"
-alias date.date="date --iso-8601=date"
+if [ `uname -s` = Darwin ]; then
+  alias date="date '+%Y-%m-%dT%T+0900'"
+  alias date.minutes="\\date '+%Y-%m-%dT%H:%M+0900'"
+  alias date.date="\\date '+%Y-%m-%d'"
+  alias date.locale="\\date"
+else
+  alias date.minutes="date --iso-8601=minutes"
+  alias date.date="date --iso-8601=date"
+fi
 
 alias ntpdate-hwclock="sudo ntpdate 210.173.160.27 && sudo hwclock --systohc"
 if [ `uname -s` = Darwin ]; then
